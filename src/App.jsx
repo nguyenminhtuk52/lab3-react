2c5d0897417731ef50a9eebd6a34fce828a27b6b
Tu Nguyen <nguyenminhtuk52@gmail.com>
2022-07-29 23:45:39 +0700

    add file

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Product from './components/product'
+import Cart from './components/cart'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [products, setProducts] = useState([])
+  const fetchProduct = async function() {
+    const data = await(await fetch('https://62e09871fa8ed271c4851d98.mockapi.io/products')).json()
+    setProducts(data)
+  }
+  useEffect(() => {
+    fetchProduct()
+  }, [])
+  return (
+    <div>
+      <Product product={products}/>
+      <Cart/>
+    </div>
+  )
+}
+
+export default App
